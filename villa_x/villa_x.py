6ee64472be6678bda6417114d38a8bfed8b86f58
lucidrains <lucidrains@gmail.com>
2025-08-09 09:47:24 -0700

    both the act-latent and act-robot are doing diffusion / flow, just in a hierarchical fashion

--- villa_x/villa_x.py
+++ villa_x/villa_x.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import torch
 from torch.nn import Module
 
@@ -16,6 +18,76 @@ def exists(v):
 
 def default(v, d):
     return v if exists(v) else d
+
+# flow DiT
+
+# random sinusoidal for times
+
+class RandomSinusoidalPosEmb(Module):
+    def __init__(self, dim):
+        super().__init__()
+        assert divisible_by(dim, 2)
+        half_dim = dim // 2
+        self.weights = nn.Parameter(torch.randn(half_dim), requires_grad = False)
+
+    def forward(self, x):
+        freqs = x * rearrange(self.weights, 'd -> 1 d') * 2 * torch.pi
+        fouriered = torch.cat((freqs.sin(), freqs.cos()), dim = -1)
+        return fouriered
+
+# DiT wrapper
+
+class FlowTransformerWrapper(Module):
+    def __init__(
+        self,
+        dim_input,
+        dim_time,
+        transformer: Encoder
+    ):
+        super().__init__()
+
+        self.transformer = transformer
+
+        dim = transformer.dim
+
+        self.proj_in = nn.Linear(dim_input, dim)
+
+        self.to_time_cond = nn.Sequential(
+            RandomSinusoidalPosEmb(dim),
+            nn.Linear(dim, dim_time),
+            nn.SiLU(),
+        )
+
+        self.proj_out = nn.Linear(dim, dim_input)
+
+    def forward(
+        self,
+        actions,
+        *,
+        times,
+        context = None,
+        context_mask = None,
+        vlm_key_values = None,
+        vlm_seq_mask = None
+    ):
+        batch_size = actions.shape[0]
+
+        time_cond = self.to_time_cond(times)
+
+        tokens = self.proj_in(actions)
+
+        attended = self.transformer(
+            tokens,
+            condition = condition,
+            context = context,
+            context_mask = context_mask,
+            self_attn_additional_kv = vlm_key_values,
+            detach_additional_kv = True,
+            additional_kv_mask = vlm_seq_mask
+        )
+
+        pred = self.proj_out(attended)
+        return pred
 
 # ACT latent
 
@@ -37,20 +109,20 @@ class LatentActionModel(Module):
 class ACTLatent(Module):
     def __init__(
         self,
-        encoder: Encoder
+        flow_dit: FlowTransformerWrapper
     ):
         super().__init__()
-        self.encoder = encoder
-        self.flow = RectifiedFlow(encoder)
+        self.flow_dit = flow_dit
+        self.flow_wrapper = RectifiedFlow(flow_dit)
 
 class ACTRobot(Module):
     def __init__(
         self,
-        encoder: Encoder
+        flow_dit: FlowTransformerWrapper
     ):
         super().__init__()
-        self.encoder = encoder
-        self.flow = RectifiedFlow(encoder)
+        self.flow_dit = flow_dit
+        self.flow_wrapper = RectifiedFlow(flow_dit)
 
 # the main class
 
